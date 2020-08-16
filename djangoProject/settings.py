5e4e268d70b39806cef66aae69b1ce908cb4cd85
sac2022 <sac2022@users.noreply.github.com>
2020-08-16 23:57:49 +0530

    Ready for Heroku

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -133,10 +133,7 @@ LOGGING = {
 # or allow read-only access for unauthenticated users.
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-
-}
+    }
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
