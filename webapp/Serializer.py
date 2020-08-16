3e965877f91be83fd0954ac0d26db7baeac26885
sac2022 <sac2022@users.noreply.github.com>
2020-08-16 15:49:11 +0530

    modify serializer to include activity_periods

--- webapp/Serializer.py
+++ webapp/Serializer.py
@@ -1,27 +1,17 @@
-from abc import ABC
-
-import pytz
-import six
-from django.utils import timezone
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
-
 from .models import Member, Period
 
 
-class PeriodSerializer(serializers.HyperlinkedModelSerializer):
+class PeriodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Period
-        fields = '__all__'
+        fields = ['start', 'end']
+
 
 # Serializing the data from the model
-class ItemSerializer(serializers.HyperlinkedModelSerializer):
+class ItemSerializer(serializers.ModelSerializer):
     activity_periods = PeriodSerializer(many=True, read_only=True)
 
     class Meta:
-        model = Member  # model name
-        # fields = ('mem_id', 'real_name', 'tz')
+        model = Member
         fields = ['mem_id', 'real_name', 'tz', 'activity_periods']
-
-
-
